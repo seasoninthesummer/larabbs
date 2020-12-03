21ac3e4b2812ad5a508a7b83ad6292a97ebc606a
seasoninthesummer <1511947301@qq.com>
2020-12-03 06:41:24 +0000

    话题内容页面

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -26,15 +26,9 @@ class TopicsController extends Controller
         return view('topics.index', compact('topics'));
     }
 
-    public function show(Category $category, Request $request, Topic $topic)
+    public function show(Topic $topic)
     {
 
-        // 读取分类 ID 关联的话题，并按每 20 条分页
-        $topics = $topic->withOrder($request->order)
-            ->where('category_id', $category->id)
-            ->paginate(20);
-
-        // 传参变量话题和分类到模板中
         return view('topics.show', compact('topic'));
     }
 
