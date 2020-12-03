947193ef8198efa54da9b97ef29d7b1f48d71899
seasoninthesummer <1511947301@qq.com>
2020-12-03 07:57:02 +0000

    翻译URL标示

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -26,9 +26,13 @@ class TopicsController extends Controller
         return view('topics.index', compact('topics'));
     }
 
-    public function show(Topic $topic)
+    public function show(Request $request, Topic $topic)
     {
 
+        // URL 矫正
+        if (!empty($topic->slug) && $topic->slug != $request->slug) {
+            return redirect($topic->link(), 301);
+        }
         return view('topics.show', compact('topic'));
     }
 
@@ -43,7 +47,7 @@ class TopicsController extends Controller
         $topic->fill($request->all());
         $topic->user_id = Auth::id();
         $topic->save();
-        return redirect()->route('topics.show', $topic->id)->with('success', '帖子创建成功！');
+        return redirect()->route($topic->link())->with('success', '帖子创建成功！');
     }
 
     public function edit(Topic $topic)
@@ -51,7 +55,7 @@ class TopicsController extends Controller
         $this->authorize('update', $topic);
         $categories = Category::all();
 
-        return view('topics.create_and_edit', compact('topic','categories'));
+        return view('topics.create_and_edit', compact('topic', 'categories'));
     }
 
     public function update(TopicRequest $request, Topic $topic)
@@ -59,7 +63,7 @@ class TopicsController extends Controller
         $this->authorize('update', $topic);
         $topic->update($request->all());
 
-        return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+        return redirect()->route($topic->link())->with('message', 'Updated successfully.');
     }
 
     public function destroy(Topic $topic)
