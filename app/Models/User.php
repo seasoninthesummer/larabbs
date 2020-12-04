c15574203f5cdff1d363d2366111a95676f6a677
seasoninthesummer <1511947301@qq.com>
2020-12-04 02:36:52 +0000

    回复列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,7 +28,7 @@ class User extends Authenticatable implements MustVerifyEmailContract
         return $this->id == $model->user_id;
     }
 
-    public function repies()
+    public function replies()
     {
         return $this->hasMany(Reply::class);
     }
