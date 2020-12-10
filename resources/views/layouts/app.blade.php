c0243c38d43961675cc2419a998ab5005de39686
seasoninthesummer <1511947301@qq.com>
2020-12-10 01:47:59 +0000

    站点权限部署

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
-  <meta name="description" content="@yield('description', 'LaraBBS 爱好者社区')" />
+  <meta name="description" content="@yield('description', 'LaraBBS 爱好者社区')"/>
   <!-- Styles -->
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
   @yield('styles')
@@ -20,7 +20,11 @@
     @yield('content')
   </div>
   @include('layouts._footer')
+
 </div>
+@if (app()->isLocal())
+  @include('sudosu::user-selector')
+@endif
 <!-- Scripts -->
 <script src="{{ mix('js/app.js') }}"></script>
 @yield('scripts')
