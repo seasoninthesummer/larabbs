ae5ed6c1775d633602792a0fd5bd5ea5a732278a
seasoninthesummer <1511947301@qq.com>
2020-12-10 00:36:35 +0000

    集成 laravel-permission扩展包

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,11 @@ use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Auth;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements MustVerifyEmailContract
 {
-    use Notifiable, MustVerifyEmailTrait;
+    use Notifiable, MustVerifyEmailTrait,HasRoles;
 
     protected $fillable = [
         'name', 'email', 'password', 'introduction', 'avatar',
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -44,5 +44,15 @@ class UsersTableSeeder extends Seeder
         $user->avatar = 'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png';
         $user->save();
 
+
+        //初始化用户角色，将1号用户指派为站长
+        $user->assignRole('Founder');
+
+        // 讲2号用户指派为管理员
+        $user = User::find(2);
+        $user->assignRole('Maintainer');
+
+
     }
 }
+
